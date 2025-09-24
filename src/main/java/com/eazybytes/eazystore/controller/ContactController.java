2d16b163271339c6af01a74a9196fe5dd50bfa18
Bayram-Y <bayramyaxiyaev7@gmail.com>
2025-09-24 15:05:22 +0900

    feat:

---
+++ src/main/java/com/eazybytes/eazystore/controller/ContactController.java
@@ -0,0 +1,26 @@
+package com.eazybytes.eazystore.controller;
+
+import com.eazybytes.eazystore.dto.ContactRequestDto;
+import com.eazybytes.eazystore.service.IContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/contacts")
+@RequiredArgsConstructor
+public class ContactController {
+
+    private final IContactService iContactService;
+
+    @PostMapping
+    public String saveContact(@RequestBody ContactRequestDto contactRequestDto)  {
+        boolean isSaved = iContactService.saveContact(contactRequestDto);
+        if (isSaved) {
+            return  "Request processed successfully";
+        } else {
+            return "An error occured. Please try again or contact Dev team";
+        }
+    }
+}
