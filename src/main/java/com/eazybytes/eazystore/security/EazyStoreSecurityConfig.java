572c6041ae6b85641f9bdeefbdc95f26d04330bb
Bayram-Y <bayramyaxiyaev7@gmail.com>
2025-11-05 20:28:58 +0900

    feat: developing Authentication  process

--- src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -3,13 +3,20 @@ package com.eazybytes.eazystore.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -35,6 +42,29 @@ public class EazyStoreSecurityConfig {
                 )
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults()).build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user1 = User.builder().username("madan").password("eazystore").roles("USER").build();
+        var user2 = User.builder().username("admin").password("$2a$12$NtiXyJF4wo/G4gmPBrv1tet3e6Fa0z1K.4HkyUAimpWAvqNkEvgXO").roles("USER", "ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        var daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        var providerManager = new ProviderManager(daoAuthenticationProvider);
+        return providerManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
