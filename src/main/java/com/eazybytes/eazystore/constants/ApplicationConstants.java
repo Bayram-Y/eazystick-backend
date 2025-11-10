fbf1a2b7077b7019d58eef7799065a7a8cb7c300
Bayram-Y <bayramyaxiyaev7@gmail.com>
2025-11-10 20:46:20 +0900

    feat: developed Profile Api

--- src/main/java/com/eazybytes/eazystore/constants/ApplicationConstants.java
+++ src/main/java/com/eazybytes/eazystore/constants/ApplicationConstants.java
@@ -8,4 +8,5 @@ public class ApplicationConstants {
 
     public static final String JWT_SECRET_KEY = "JWT_SECRET";
     public static final String JWT_SECRET_DEFAULT_VALUE = "sadbbfreSDHBAHDHAJSjhsfdhidsSDA32423SKHD7";
+    public static final String JWT_HEADER = "Authorization";
 }
